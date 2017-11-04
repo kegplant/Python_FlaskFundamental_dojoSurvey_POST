44c2cb1a89cd7195ee40b5360fc90b60222401e5
kegplant <ace1905@gmail.com>
2017-11-04 15:06:21 -0700

    with validation

--- dojoSurvey.py
+++ dojoSurvey.py
@@ -1,12 +1,22 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, flash,session
 app = Flask(__name__)
+app.secret_key='piece of shit'
 # our index route will handle rendering our form
 @app.route('/')
 def index():
   return render_template("index.html")
 @app.route('/result', methods=['POST'])
 def result():
-
-    return render_template('result.html', info=request.form)
+  names=request.form['name']
+  if len(names)<1:
+    flash('Name cannot be empty!')
+    return redirect('/')
+  if len(request.form['comment'])<1:
+    flash('Comment cannot be empty!')
+    return redirect('/')
+  elif len(request.form['comment'])>120:
+    flash('Please keep comments under 120 characters.')
+    return redirect('/')
+  return render_template('result.html', info=request.form)
 
 app.run(debug=True)
